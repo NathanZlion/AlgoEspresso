96209520edf64172404216826f547120fa191ff4
Nathnael Dereje <nathandere1357@gmail.com>
2025-02-05 02:26:21 +0300

    Rename frontend to Algoespresso, Complete hero section

---
+++ algoespresso_frontend/src/app/layout.tsx
@@ -0,0 +1,55 @@
+import type { Metadata } from "next";
+import LocalFont from "next/font/local"
+import "./globals.css";
+import { ThemeProvider } from "@/components/ui/theme-provider";
+
+
+const dotGothic16 = LocalFont(
+  {
+    src: "./fonts/DotGothic16-Regular.ttf",
+    variable: "--font-dot-gothic",
+    weight: "400",
+  }
+)
+
+const AzeretMono = LocalFont(
+  {
+    src: [
+      {
+        path: "./fonts/azeret_mono/AzeretMono-Regular.ttf",
+        weight: "400"
+      },
+      {
+        path: "./fonts/azeret_mono/AzeretMono-Bold.ttf",
+        weight: "700"
+      }
+    ],
+    variable: "--font-azeret-mono",
+  }
+)
+
+export const metadata: Metadata = {
+  title: "AlgoEspresso",
+  description: "Best place to learn DSA",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${AzeretMono.variable} ${dotGothic16.variable} antialiased bg-background`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html >
+  );
+}
---
+++ algoespresso_frontend/src/components/ui/magic-button.tsx
@@ -0,0 +1,48 @@
+import { cn } from "@/lib/utils";
+import { cva, VariantProps } from "class-variance-authority";
+import React from "react"
+
+
+const MagicButtonVariants = cva(
+    "",
+    {
+        variants: {
+            roundness: {
+                default: "rounded-full",
+                sm: "rounded-sm",
+                md: "rounded-md",
+                lg: "rounded-lg",
+                full: "rounded-full"
+            }
+        },
+        defaultVariants: {
+            roundness: "default",
+        }
+    }
+)
+
+export interface MagicButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement>, VariantProps<typeof MagicButtonVariants> { };
+
+const MagicButton = React.forwardRef<HTMLButtonElement, MagicButtonProps>(
+    ({ className, children, roundness, ...props }, ref) => {
+        return (
+            <button
+                className={cn(MagicButtonVariants({ roundness, className }),
+                    "relative inline-flex h-12 overflow-hidden p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50",
+                )}
+                ref={ref}
+                {...props}
+            >
+                <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+
+                <span className={cn(MagicButtonVariants({ roundness, className }),
+                    "inline-flex h-full w-full cursor-pointer items-center justify-center  bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl",
+                )}>
+                    {children}
+                </span>
+            </button >
+        )
+    }
+)
+
+export { MagicButton }
