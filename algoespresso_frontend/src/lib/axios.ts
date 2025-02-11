99bd2dbe8a0993f069f2923debd01de24eeb9617
Nathnael Dereje <nathandere1357@gmail.com>
2025-02-11 19:03:51 +0300

    Add axios interceptor to add token to request

---
+++ algoespresso_frontend/src/lib/axios.ts
@@ -0,0 +1,16 @@
+import axios from "axios";
+import { getAuthToken } from "./utils";
+
+console.log(process.env.NEXT_PUBLIC_BACKEND_URL)
+
+export const apiClient = axios.create({
+    baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
+})
+
+apiClient.interceptors.request.use(async (config) => {
+    config.headers.Authorization = `Bearer ${await getAuthToken()}`
+    console.log(config.headers.Authorization)
+    return config;
+}, (err: Error) => {
+    console.log(err)
+});
